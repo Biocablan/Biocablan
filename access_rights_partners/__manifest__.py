6f4d5b8731a655d52d65b0dd0251bc2234365473
Raül Carbonell <raul.carbonell@processcontrol.es>
2020-04-03 18:48:14 +0200

    [ADD] Acces Rights to Partners

---
+++ access_rights_partners/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Raul Carbonell - raul.carbonell@processcontrol.es
+# License AGPL-3.0 or later
+{
+    "name": "Access Rights to Partners",
+    "summary":
+        "Crear Permisos de Acceso Para los Partners",
+    "version": "13.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "website": "",
+    "author": "Raul Carbonell",
+    "license": "AGPL-3",
+    "data": [
+        "security/res_partner_access_rights_security.xml",
+        "security/ir.model.access.csv",
+    ],
+    "depends": [
+    ],
+    "application": True,
+    "installable": True,
+}
